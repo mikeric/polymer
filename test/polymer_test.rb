3fef78a8f8f39c58cbdc58f1ce83802d1318348a
Mike Richards <mike22e@gmail.com>
2009-07-05 18:35:59 -0400

    Rewrote unit tests to fit with body property.

--- test/polymer_test.rb
+++ test/polymer_test.rb
@@ -5,12 +5,22 @@ require 'helpers'
 class PolymerTest < Test::Unit::TestCase
   context "A Polyrhythm" do
     setup do
-      @polyrhythm = Polyrhythm.new(:patterns => [{:time => 4, :pattern => 'o-oo-oo-'},
-                                                 {:time => 6, :pattern => 'oo-o-o-o--o-'}])
+      @polyrhythm = Polyrhythm.new(:patterns => [{:time => 4, :body => 'x-x-x-x-'},
+                                                 {:time => 6, :body => 'o-o-o-o-o-o-'}])
     end
     
     should "get resolved at the lowest number equally divisible by all patterns" do
       assert_equal @polyrhythm.resolve, 12
+    end
+  end
+  
+  context "A Pattern" do
+    setup do
+      @pattern = Pattern.new(:time => 4, :body => 'o-o-o-o-')
+    end
+    
+    should "contain 2 characters for each beat" do
+      assert_equal @pattern.body.length, @pattern.time * 2
     end
   end
 end
