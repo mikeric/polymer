a4c6d9ab7556610dfa3d9c45c317604d740caf7a
Mike Richards <mike22e@gmail.com>
2009-11-28 20:08:14 -0500

    Added form feedback to include time signature

--- public/javascript/polymer.js
+++ public/javascript/polymer.js
@@ -1,42 +1,42 @@
 $(document).ready(function(){
   $("#usage a").click(function(){
-    $(".pattern:first > input:first").val('4');
-    $(".pattern:first > input:last").val('o-oo-oo-');
-    $(".pattern:eq(1) > input:first").val('5');
-    $(".pattern:eq(1) > input:last").val('o--oo--o-o');
-    $(".pattern:first").addClass('valid');
-    $(".pattern:eq(1)").addClass('valid');
+    $(".pattern:first > input:first").val("4");
+    $(".pattern:first > input:last").val("o-oo-oo-");
+    $(".pattern:eq(1) > input:first").val("5");
+    $(".pattern:eq(1) > input:last").val("o--oo--o-o");
+    $(".pattern:first").addClass("valid");
+    $(".pattern:eq(1)").addClass("valid");
     return false;
   });
   
   $("input[value=+]").click(function(){
     $(".pattern:last").clone().insertAfter(".pattern:last").hide();
-    $(".pattern:last > input").val('');
-    $(".pattern:last").removeClass('valid');
-    $(".pattern:last").removeClass('invalid');
-    $(".pattern:last").show('slow');
+    $(".pattern:last > input").val("");
+    $(".pattern:last").removeClass("valid");
+    $(".pattern:last").removeClass("invalid");
+    $(".pattern:last").show("slow");
   });
   
-  $("input[type=text]").live('keyup', function(){
+  $("input[type=text]").live("keyup", function(){
     var pattern = $(this).parent();
-    var time = pattern.find("input.time");
+    var time = pattern.find("select.time");
     var body = pattern.find("input.body");
     
-    if(time.val() < 1){
-      pattern.removeClass('valid');
-    }else if(body.val().length == time.val() * 2){
-      pattern.removeClass('invalid');
-      pattern.addClass('valid');
+    if(body.val().length % (16 / time.val()) == 0){
+      pattern.removeClass("invalid");
+      pattern.addClass("valid");
+      pattern.find(".timesig").text((body.val().length / (16 / time.val())) + '/' + time.val());
     }else{
-      pattern.removeClass('valid');
+      pattern.removeClass("valid");
+      pattern.find(".timesig").text("");
     }
   });
   
   $("form").submit(function(){
     var valid = true;
-    $('.pattern').each(function(){
-      if(!$(this).hasClass('valid')){
-        $(this).addClass('invalid');
+    $(".pattern").each(function(){
+      if(!$(this).hasClass("valid")){
+        $(this).addClass("invalid");
         valid = false;
       }
     });
