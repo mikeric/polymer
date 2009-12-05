b1e532b5bace211beec1361ff71d6ed4982dd29b
Mike Richards <mike22e@gmail.com>
2009-12-05 02:59:43 -0500

    Reimplemented jQuery validation function. Changed usage details.

--- public/javascript/polymer.js
+++ public/javascript/polymer.js
@@ -1,40 +1,49 @@
+function validate(pattern){
+  var time = pattern.find("select.time");
+  var body = pattern.find("input.body");
+  
+  if(body.val().length != 0 && body.val().length % (16 / time.val()) == 0){
+    pattern.removeClass("invalid");
+    pattern.addClass("valid");
+    pattern.find(".timesig").text((body.val().length / (16 / time.val())) + '/' + time.val());
+  }else{
+    pattern.removeClass("invalid");
+    pattern.removeClass("valid");
+    pattern.find(".timesig").text("");
+  }
+}
+
 $(document).ready(function(){
   $("#usage a").click(function(){
-    $(".pattern:first > input:first").val("4");
-    $(".pattern:first > input:last").val("o-oo-oo-");
-    $(".pattern:eq(1) > input:first").val("5");
-    $(".pattern:eq(1) > input:last").val("o--oo--o-o");
-    $(".pattern:first").addClass("valid");
-    $(".pattern:eq(1)").addClass("valid");
+    $(".pattern:eq(0) > select.time").val("4");
+    $(".pattern:eq(0) > input.body").val("x---x---o---");
+    $(".pattern:eq(1) > select.time").val("16");
+    $(".pattern:eq(1) > input.body").val("o-o--");
+    validate($(".pattern:eq(0)"));
+    validate($(".pattern:eq(1)"));
     return false;
   });
   
   $("input[value=+]").click(function(){
     $(".pattern:last").clone().insertAfter(".pattern:last").hide();
-    $(".pattern:last > input").val("");
-    $(".pattern:last").removeClass("valid");
-    $(".pattern:last").removeClass("invalid");
+    $(".pattern:last > select.time").val("4");
+    $(".pattern:last > input.body").val("");
+    validate($(".pattern:last"));
     $(".pattern:last").show("slow");
   });
   
-  $("input[type=text]").live("keyup", function(){
-    var pattern = $(this).parent();
-    var time = pattern.find("select.time");
-    var body = pattern.find("input.body");
-    
-    if(body.val().length % (16 / time.val()) == 0){
-      pattern.removeClass("invalid");
-      pattern.addClass("valid");
-      pattern.find(".timesig").text((body.val().length / (16 / time.val())) + '/' + time.val());
-    }else{
-      pattern.removeClass("valid");
-      pattern.find(".timesig").text("");
-    }
+  $(".pattern input.body").live("keyup", function(){
+    validate($(this).parent());
+  });
+  
+  $(".pattern select.time").change(function(){
+    validate($(this).parent());
   });
   
   $("form").submit(function(){
     var valid = true;
     $(".pattern").each(function(){
+      validate($(this));
       if(!$(this).hasClass("valid")){
         $(this).addClass("invalid");
         valid = false;
