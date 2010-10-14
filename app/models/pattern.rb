bf1b8eeb3a52d19a6830e047e13b4e04f0002090
Mike Richards <mike22e@gmail.com>
2010-10-14 01:35:16 -0700

    Added compositions and patterns previously implemented in Sinatra application.

---
+++ app/models/pattern.rb
@@ -0,0 +1,25 @@
+class Pattern
+  include MongoMapper::EmbeddedDocument
+  
+  key :resolution,  Integer,  :required => true, :default => 16
+  key :time,        Integer,  :required => true
+  key :body,        String,   :required => true
+  
+  validate :body_length
+  
+  def beats
+    body.length / (resolution / time)
+  end
+  
+  def time_signature
+    "#{beats}/#{time}"
+  end
+  
+  private
+  
+  def body_length
+    if body.length % (resolution / time) != 0
+      errors.add(:body, "must be devisible by the beat resolution")
+    end
+  end
+end
