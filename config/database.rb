f410e4db212fd1e2cf22eb2e786def59c7442833
Mike Richards <mike22e@gmail.com>
2010-10-15 00:41:41 -0700

    Reconfigured database initializer.

--- config/database.rb
+++ config/database.rb
@@ -1,7 +1,7 @@
-MongoMapper.connection = Mongo::Connection.new('localhost', nil, :logger => logger)
-
-case Padrino.env
-  when :development then MongoMapper.database = 'polymer_development'
-  when :production  then MongoMapper.database = 'polymer_production'
-  when :test        then MongoMapper.database = 'polymer_test'
+if ENV['MONGOHQ_URL']
+  MongoMapper.config = {PADRINO_ENV => {'uri' => ENV['MONGOHQ_URL']}}
+else
+  MongoMapper.config = {PADRINO_ENV => {'uri' => "mongodb://localhost/padrino-#{PADRINO_ENV}"}}
 end
+
+MongoMapper.connect(PADRINO_ENV)
