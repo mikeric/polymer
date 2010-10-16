a50d23d2c4a7d9bb207a5b0cd45235b30a34118f
Mike Richards <mike22e@gmail.com>
2010-10-16 16:52:42 -0700

    Structural changes in front-end and measure rendering.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 Polymer.helpers do
+  def render_composition(comp, measures=1)
+    comp.patterns.map do |pattern|
+      tagged = pattern.body.split(//)
+      tagged[0] = "<span>#{tagged[0]}</span>"
+      measure = tagged * comp.bars(pattern) * measures
+      measure.enum_for(:each_slice, comp.base.body.length).map{|bar| bar.unshift("|").join}
+    end.transpose.in_groups_of(90 / comp.base.body.length, false).map(&:transpose)
+  end
 end
